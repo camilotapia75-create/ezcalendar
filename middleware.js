943e0a2f8961d3b8c2a6f3557aa30e214452dbb2
camilotapia75-create <camilotapia75@gmail.com>
2026-05-22 22:48:04 -0700

    fix: exclude /auth/ from middleware to prevent PKCE interference

--- middleware.js
+++ middleware.js
@@ -33,5 +33,5 @@ export async function middleware(request) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|api/).*)']
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|api/|auth/).*)'],
 }
